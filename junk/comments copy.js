9a5e45b6de172f339846d582c48e819b0b20dc4d
Emilee Hatch <87101698+emt0rres@users.noreply.github.com>
2022-06-04 23:30:17 -0600

    Updates, week 07 notes

---
+++ junk/comments copy.js
@@ -0,0 +1,16 @@
+class Comments {
+    constructor(hikeName, date, content) {
+        this.hikeName = hikeName;
+        this.date = date;
+        this.content = content;
+    }
+    getAllComments(){
+
+    }
+    renderCommentList(){
+
+    }
+    filterCommentByName(){
+
+    }
+}
