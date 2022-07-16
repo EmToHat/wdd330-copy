de015dbc1b4e3f156239612447f9067e0450131e
Emilee Hatch <87101698+emt0rres@users.noreply.github.com>
2022-07-16 06:58:38 -0600

    Updates to Delicioso Project

--- challenges/recipes/js/app.js
+++ challenges/recipes/js/app.js
@@ -5,4 +5,12 @@ function myFunction() {
     } else {
       x.className = "topnav";
     }
-  }
+}
+
+
+let lastUpdate = document.lastModified;
+document.getElementById("modified").innerHTML = lastUpdate;
+
+const date = new Date();
+const year = date.getFullYear();
+document.getElementById("year").innerHTML = year;
